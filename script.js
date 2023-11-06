27d8bc5cad5ad2675dafdeadb91c6c6a9e0fbde7
huanchin <cherry032826@gmail.com>
2023-11-06 21:07:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -135,3 +135,73 @@ console.log([...question]);
 console.log([...question.entries()]);
 console.log([...question.keys()]);
 console.log([...question.values()]);
+
+/**** Coding Challenge #3 ****/
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// 1.
+// const evnetArr = [];
+// for (const event of gameEvents.values()) {
+//   evnetArr.push(event);
+// }
+// console.log(evnetArr);
+// const event = new Set(evnetArr);
+// console.log(event);
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+const eventTime = [...gameEvents.keys()].pop();
+const average = eventTime / gameEvents.size;
+console.log(`An event happened, on average, every ${average} minutes`);
+
+// 4.
+// for (const event of gameEvents) {
+//   if (event[0] < 45) {
+//     console.log(`[FIRST HALF] ${event[0]} ${event[1]}`);
+//   } else {
+//     console.log(`[SECOND HALF] ${event[0]} ${event[1]}`);
+//   }
+// }
+
+// for (const [key, value] of gameEvents) {
+//   if (key < 45) {
+//     console.log(`[FIRST HALF] ${key} ${value}`);
+//   } else {
+//     console.log(`[SECOND HALF] ${key}: ${value}`);
+//   }
+// }
+
+for (const [min, event] of gameEvents) {
+  const half = min < 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
